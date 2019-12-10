844c26bc7bd7c237e1d2e5ec7c1f7b0ed0d5ea86
thnikk <thnikk@gmail.com>
2019-12-10 15:24:21 -0800

    Who knows

--- chroot.sh
+++ chroot.sh
@@ -3,17 +3,21 @@ ROOT=/dev/vda3
 
 passwd
 # PROMPT FOR USERNAME
-useradd -mg users -G wheel,storage,power -s /usr/bin/zsh thnikk
-passwd thnikk
+echo "Please enter your desired username: "
+read USER
+useradd -mg users -G wheel,storage,power -s /usr/bin/zsh $USER 
+passwd $USER
 #visudo
 sudo sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 # PROMPT FOR HOSTNAME
-echo "archvm" > /etc/hostname
+echo "Please enter your desired hostname for the machine: "
+read HOSTNAME
+echo "$HOSTNAME" > /etc/hostname
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
-locale-gen >> /dev/null
 export LANG=en_US.UTF-8
+locale-gen
 ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc --utc
 
@@ -34,4 +38,5 @@ echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=PARTUUID=$(blkid -s PARTUUID -o value $ROOT | tr -d '\n') rw" >> /boot/loader/entries/arch.conf
 
+chmod +x /user.sh
 su -c "/user.sh" - thnikk 
--- user.sh
+++ user.sh
@@ -1,9 +1,9 @@
-git clone https://aur.archlinux.org/yay.git
+gt clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ../
 git clone https://github.com/LukeSmithxyz/st.git
 makepkg -si
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 touch /home/thnikk/.xinitrc
 echo "i3" > /home/thnikk/.xinitrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
