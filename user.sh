965cabc97856e8bc1218037910b0ee2b5ae7ac8d
thnikk <thnikk@github.com>
2019-12-11 12:53:00 -0800

    Added dotfile install

--- user.sh
+++ user.sh
@@ -1,10 +1,15 @@
-
+#!/bin/bash
+# Install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S st-luke-git flat-remix polybar ttf-material-design-icons
+# Install aur packages
+yay -S flat-remix polybar ttf-material-design-icons oh-my-zsh
+# Create xinitrc
 touch /home/thnikk/.xinitrc
-echo "exec i3" > /home/thnikk/.xinitrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+echo "exec bspwm" > /home/thnikk/.xinitrc
+# Just in case
+echo "#exec i3" >> /home/thnikk/.xinitrc
+# Install dotfiles
+git clone https://github.com/thnikk/dotfiles.git
+cp -r dotfiles/. .
