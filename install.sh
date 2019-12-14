b6ac21752dbcdc55479ef87864c1a3b30cf754a2
thnikk <thnikk@github.com>
2019-12-14 15:45:41 -0800

    Removed unnecessary line making fstab

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 lsblk
-echo "Enter drive selection."
+echo "WARNING: THIS WILL OVERWRITE THE SELECTED DRIVE."
+echo "Enter drive selection without partition number."
 read DRIVE
 
 ROOT=/dev/$(echo $DRIVE)3
@@ -22,10 +23,9 @@ echo "Installing system."
 pacman -Sy --noconfirm archlinux-keyring pacman-contrib
 rankmirrors -n 6 mirrorlist > /etc/pacman.d/mirrorlist
 
-pacstrap /mnt base base-devel linux linux-firmware neovim zsh dhcpcd xorg-server xorg xorg-xinit sudo noto-fonts git feh unzip unrar tmux xclip mpd mpc ncmpcpp networkmanager network-manager-applet arc-gtk-theme adobe-source-han-sans-otc-fonts i3-gaps bspwm sxhkd rofi dunst udiskie xorg-xsetroot xorg-xinput arandr ttf-fira-code nautilus kitty chromium yad pulseaudio
+pacstrap /mnt base base-devel linux linux-firmware neovim zsh dhcpcd xorg-server xorg xorg-xinit sudo noto-fonts git feh unzip unrar tmux xclip mpd mpc ncmpcpp networkmanager network-manager-applet arc-gtk-theme adobe-source-han-sans-otc-fonts i3-gaps bspwm sxhkd rofi dunst udiskie xorg-xsetroot xorg-xinput arandr ttf-fira-code nautilus kitty chromium yad pulseaudio maim slop xclip openssh pamixer
 echo "Creating FS Table."
-rm /mnt/etc/fstab
-genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt > /mnt/etc/fstab
 echo "Copying install scripts to root fs and entering chroot."
 echo "$ROOT" > /mnt/rootPart
 cp chroot.sh /mnt && cp user.sh /mnt && arch-chroot /mnt bash chroot.sh && cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
