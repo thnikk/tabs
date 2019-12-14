9186fac836f4bab72fc281e5371b78aa0fa32696
thnikk <thnikk@github.com>
2019-12-13 17:26:22 -0800

    cd to ~ before dotfile install

--- user.sh
+++ user.sh
@@ -6,6 +6,7 @@ makepkg -si
 # Install aur packages
 yay -S flat-remix polybar ttf-material-design-icons antigen-git
 # Install dotfiles
+cd ~
 git clone https://github.com/thnikk/dotfiles.git
 rm -rf dotfiles/.git
 cp -r dotfiles/. .
