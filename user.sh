f6d856cc36ef4231c8a82b9c00cc9553253ab5ad
thnikk <thnikk@github.com>
2020-03-20 14:41:33 -0700

    Fixed sed and added cleanup

--- user.sh
+++ user.sh
@@ -9,7 +9,9 @@ yay -S flat-remix flat-remix-gtk polybar ttf-material-design-icons antigen-git m
 cd ~
 git clone https://github.com/thnikk/dotfiles.git
 rm -rf dotfiles/.git
-sed 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.config/polybar/config
-sed 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.kitty/kitty.conf
-sed 's/Operator Mono Book/Fira Code Medium/g' dotfiles/.config/i3/config
+sed -i 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.config/polybar/config
+sed -i 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.config/kitty/kitty.conf
+sed -i 's/Operator Mono Book/Fira Code Medium/g' dotfiles/.config/i3/config
 cp -r dotfiles/. .
+# Cleanup
+rm -rf dotfiles yay
