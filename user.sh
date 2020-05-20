6218a2758e650768863db7e5bb1706d9853067d6
thnikk <thnikk@github.com>
2020-05-20 14:27:39 -0700

    Fixed sed replace for fonts

--- user.sh
+++ user.sh
@@ -7,12 +7,16 @@ makepkg -si
 yay -S --noconfirm polybar ttf-material-design-icons i3lock-fancy-rapid-git breeze-snow-cursor-theme autotiling gohufont lf
 # Install dotfiles
 cd ~
-git clone https://github.com/thnikk/dotfiles.git
-rm -rf dotfiles/.git
+git clone https://github.com/thnikk/dotfiles.git .dot
+cd .dot
 # Replace all references to Operator Mono with Firacode
-sed -i 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.config/polybar/config
-sed -i 's/Operator Mono Medium/Fira Code Medium/g' dotfiles/.config/kitty/kitty.conf
-sed -i 's/Operator Mono Book/Fira Code Medium/g' dotfiles/.config/i3/config
-cp -r dotfiles/. .
+sed -i 's/Operator Mono Book/Fira Code Medium/g' polybar/.config/polybar/config
+sed -i 's/Operator Mono Lig Book/Fira Code Medium/g' kitty/.config/kitty/kitty.conf
+sed -i 's/Operator Mono Book/Fira Code Medium/g' i3/.config/i3/config
+# Stow all
+chmod +x stowAll.sh
+./stowAll.sh
+# Return home
+cd ~
 # Cleanup
-rm -rf dotfiles yay
+rm -rf yay
