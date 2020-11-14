2da4619dfcb68eced81850c40243f1606c6e0b2b
thnikk <thnikk@users.noreply.github.com>
2020-11-14 03:26:36 -0800

    Fixed dotfile cd

--- user.sh
+++ user.sh
@@ -7,10 +7,7 @@ makepkg -si
 paru -S --noconfirm polybar breeze-snow-cursor-theme autotiling lf shellcheck-bin picom-ibhagwan-git
 # Install through pip to reduce dependencies
 pip install fontawesome
-# Install dotfiles
-cd ~
-git clone --recursive https://github.com/thnikk/dotfiles.git .dot
-cd .dot
+
 # Replace all references to Operator Mono with Firacode
 #sed -i 's/Operator Mono Medium/Fira Code Medium/g' polybar/.config/polybar/config
 #sed -i 's/Operator Mono Lig Book/Fira Code Medium/g' kitty/.config/kitty/kitty.conf
@@ -24,6 +21,10 @@ wget "https://thnikk.moe/fonts/Operator Mono Book Italic.otf"
 wget "https://thnikk.moe/fonts/OperatorMonoLig-Book.otf"
 wget "https://thnikk.moe/fonts/Operator Mono Medium Regular.otf"
 
+# Install dotfiles
+cd ~
+git clone --recursive https://github.com/thnikk/dotfiles.git .dot
+cd .dot
 # Stow all
 chmod +x stowAll.sh
 ./stowAll.sh
