3c191977269226dd66c29f727defc4764422cd10
thnikk <thnikk@users.noreply.github.com>
2020-11-14 02:56:58 -0800

    Added fonts

--- user.sh
+++ user.sh
@@ -12,9 +12,18 @@ cd ~
 git clone --recursive https://github.com/thnikk/dotfiles.git .dot
 cd .dot
 # Replace all references to Operator Mono with Firacode
-sed -i 's/Operator Mono Medium/Fira Code Medium/g' polybar/.config/polybar/config
-sed -i 's/Operator Mono Lig Book/Fira Code Medium/g' kitty/.config/kitty/kitty.conf
-sed -i 's/Operator Mono Book/Fira Code Medium/g' i3/.config/i3/config
+#sed -i 's/Operator Mono Medium/Fira Code Medium/g' polybar/.config/polybar/config
+#sed -i 's/Operator Mono Lig Book/Fira Code Medium/g' kitty/.config/kitty/kitty.conf
+#sed -i 's/Operator Mono Book/Fira Code Medium/g' i3/.config/i3/config
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget "https://thnikk.moe/fonts/FuturaBookBT.ttf"
+wget "https://thnikk.moe/fonts/Gotham-Rounded-Book.ttf"
+wget "https://thnikk.moe/fonts/Gotham-Rounded-Light.ttf"
+wget "https://thnikk.moe/fonts/Operator Mono Book Italic.otf"
+wget "https://thnikk.moe/fonts/OperatorMonoLig-Book.otf"
+wget "https://thnikk.moe/fonts/Operator Mono Medium Regular.otf"
+
 # Stow all
 chmod +x stowAll.sh
 ./stowAll.sh
