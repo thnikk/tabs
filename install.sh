a2e1f4df6324c71c6f7c900a046b7bf1c7647e7c
thnikk <thnikk@users.noreply.github.com>
2020-11-19 14:09:26 -0800

    Fixed variable formatting

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 lsblk
 echo "Enter the drive name you'd like to format. THIS WILL FORMAT THE WHOLE DRIVE."
-echo -n "Valid drive names are:"
+echo "Valid drive names are:"
 lsblk -nd --output NAME
+
 read -r DRIVE
 
-SWAPSIZE=$(($(cat /proc/meminfo | grep MemTotal | awk '{print int($2/1024)}')+300))
+SWAPSIZE=$(($(grep MemTotal /proc/meminfo | awk '{print int($2/1024)}')+300))
 # Creates boot, swap, and root partition.
-parted --script /dev/$DRIVE \
+parted --script "/dev/$DRIVE" \
     mklabel gpt \
     mkpart primary fat32 1MiB 300MiB \
     set 1 esp on \
@@ -15,29 +16,31 @@ parted --script /dev/$DRIVE \
     mkpart primary ext4 "$SWAPSIZE"MiB 100% \
     align-check min 1
 
-ROOT=/dev/$(echo $DRIVE)3
-SWAP=/dev/$(echo $DRIVE)2
-BOOT=/dev/$(echo $DRIVE)1
+ROOT=/dev/${DRIVE}3
+SWAP=/dev/${DRIVE}2
+BOOT=/dev/${DRIVE}1
+
+echo "Root: $ROOT, Swap: $SWAP, Boot: $BOOT"
 
 echo "Formatting partitions."
-mkfs.ext4 $ROOT > /dev/null
-mkswap $SWAP > /dev/null
-mkfs.fat -F32 $BOOT > /dev/null
+mkfs.ext4 "$ROOT"
+mkswap "$SWAP"
+mkfs.fat -F32 "$BOOT"
 
 echo "Mounting partitions."
-mount $ROOT /mnt > /dev/null
-mkdir /mnt/boot > /dev/null
-mount $BOOT /mnt/boot > /dev/null
-swapon $SWAP > /dev/null
+mount "$ROOT" /mnt
+mkdir /mnt/boot
+mount "$BOOT" /mnt/boot
+swapon "$SWAP"
 
 echo "Installing system."
-pacman -Sy --noconfirm archlinux-keyring pacman-contrib >/dev/null
+pacman -Sy --noconfirm archlinux-keyring pacman-contrib
 rankmirrors -n 6 mirrorlist > /etc/pacman.d/mirrorlist
 
 # Install packages from file
-pacstrap /mnt $(grep -v "#" packages | tr '\n' ' ') >/dev/null
+pacstrap /mnt "$(grep -v "#" packages | tr '\n' ' ')"
 echo "Creating FS Table."
-genfstab -U -p /mnt > /mnt/etc/fstab >/dev/null
+genfstab -U -p /mnt > /mnt/etc/fstab
 echo "Copying install scripts to root fs and entering chroot."
 echo "$ROOT" > /mnt/rootPart
 cp chroot.sh /mnt && cp user.sh /mnt && arch-chroot /mnt bash chroot.sh && cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
